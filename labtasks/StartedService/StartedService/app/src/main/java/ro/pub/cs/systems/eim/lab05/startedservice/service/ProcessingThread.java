692317dec08bb595242895f3dd02a6a907437168
zahariadaniel16 <zaharia.daniel16@gmail.com>
2017-03-24 15:53:53 +0200

    implemented taks for laboratory 05

---
+++ labtasks/StartedService/StartedService/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedservice/service/ProcessingThread.java
@@ -0,0 +1,68 @@
+package ro.pub.cs.systems.eim.lab05.startedservice.service;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Parcelable;
+
+import ro.pub.cs.systems.eim.lab05.startedservice.general.Constants;
+
+/**
+ * Created by daniel on 3/24/17.
+ */
+
+public class ProcessingThread extends Thread{
+
+    private Context context;
+
+    public ProcessingThread(Context context) {
+        this.context = context;
+    }
+
+
+    @Override
+    public void run() {
+
+        Log.d(Constants.TAG, "Thread.run() was invoked, PID: " + android.os.Process.myPid()
+                + " TID: " + android.os.Process.myTid());
+        sendMessage(Constants.MESSAGE_STRING);
+        sleep();
+        sendMessage(Constants.MESSAGE_INTEGER);
+        sleep();
+        sendMessage(Constants.MESSAGE_ARRAY_LIST);
+        sleep();
+
+    }
+
+    private void sendMessage(int messageType) {
+
+        Intent intent = new Intent();
+
+        switch(messageType) {
+
+            case Constants.MESSAGE_STRING:
+                intent.setAction(Constants.ACTION_STRING);
+                intent.putExtra(Constants.DATA, Constants.STRING_DATA);
+                break;
+            case Constants.MESSAGE_INTEGER:
+                intent.setAction(Constants.ACTION_INTEGER);
+                intent.putExtra(Constants.DATA, Constants.INTEGER_DATA);
+                break;
+            case Constants.MESSAGE_ARRAY_LIST:
+                intent.setAction(Constants.ACTION_ARRAY_LIST);
+                intent.putExtra(Constants.DATA, Constants.ARRAY_LIST_DATA);
+
+        }
+
+        context.sendBroadcast(intent);
+    }
+
+    private void sleep() {
+
+        try {
+            Thread.sleep(Constants.SLEEP_TIME);
+        } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+        }
+    }
+
+}
--- labtasks/StartedService/StartedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceActivity.java
+++ labtasks/StartedService/StartedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceActivity.java
@@ -1,5 +1,7 @@
 package ro.pub.cs.systems.eim.lab05.startedserviceactivity.view;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +23,12 @@ public class StartedServiceActivity extends AppCompatActivity {
         messageTextView = (TextView)findViewById(R.id.message_text_view);
 
         // TODO: exercise 6 - start the service
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName(
+                "ro.pub.cs.systems.eim.lab05.startedservice"
+                "ro.pub.cs.systems.eim.lab05.startedservice.service.StartedService"
+        ));
+        startService(intent);
 
         // TODO: exercise 8a - create an instance of the StartedServiceBroadcastReceiver broadcast receiver
 
